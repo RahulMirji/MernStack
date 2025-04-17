97df99d3f076e5835322c3b6382fb4f8f0fe2e90
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2025-04-17 23:22:15 +0530

    Day 02

--- 2.create-app/First-App/src/App.jsx
+++ 2.create-app/First-App/src/App.jsx
@@ -1,10 +1,12 @@
 import KgButton from "./KgButton";
+import Hello from "./Hello";
 function App(){
   return <div>
     <h1>
     Hi this is Rahul
     </h1>
     <KgButton></KgButton>
+    <Hello></Hello>
   </div>
 }
 
