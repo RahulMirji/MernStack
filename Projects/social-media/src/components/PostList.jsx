8fcf5a58a7f7c912c69c590f882ae89d838f0f9e
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2025-05-10 19:34:35 +0530

    social media useEffect done

--- Projects/social-media/src/components/PostList.jsx
+++ Projects/social-media/src/components/PostList.jsx
@@ -1,39 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const PostList = () => {
-  // Extracting the postList and addInitialPosts functions from the context
   const { postList, addInitialPosts } = useContext(PostListData);
-
-  // This function is triggered when you click the button to fetch posts
-  const handleGetPostsClick = () => {
-    fetch("https://dummyjson.com/posts")
+  const [fetching, setFetching] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    setFetching(true);
+    fetch("https://dummyjson.com/posts", { signal })
       .then((res) => res.json())
       .then((data) => {
-        addInitialPosts(data.posts); // Adding the fetched posts to the context
+        addInitialPosts(data.posts);
+        setFetching(false);
       });
-  };
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   return (
     <>
-      {/* If there are no posts, show the welcome message with a button */}
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
 
-      {/* Bootstrap Container */}
       <div className="container mt-4">
         <div className="row">
-          {/* Loop through the posts and render them in columns */}
-          {postList.map((post) => (
-            // Each Post is wrapped inside a Bootstrap column of medium size (col-md-4)
-            // "mb-4" adds margin-bottom for better spacing
-            <div className="col-md-4 mb-4" key={post.id}>
-              <Post post={post} />
-            </div>
-          ))}
+          {!fetching &&
+            postList.map((post) => (
+              <div className="col-md-4 mb-4" key={post.id}>
+                <Post post={post} />
+              </div>
+            ))}
         </div>
       </div>
     </>
--- Projects/social-media/src/components/WelcomeMessage.jsx
+++ Projects/social-media/src/components/WelcomeMessage.jsx
@@ -1,18 +1,7 @@
-const WelcomeMessage = ({onGetPostsClick}) => {
+const WelcomeMessage = () => {
   return (
     <>
-      <h1 className="welcome-message">
-        There are no Posts Yet. Click below button to fetch Posts.
-      </h1>
-
-      <button
-        type="button"
-
-        onClick={onGetPostsClick}
-        className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold welcome-message"
-      >
-        Fetch Posts form Server
-      </button>
+      <h1 className="welcome-message">There are no Posts.</h1>
     </>
   );
 };
